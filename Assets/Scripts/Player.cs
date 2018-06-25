0a0b945e46c856cc5646dba9553221a83b569a0f
Nemesh <prevter@gmail.com>
2018-06-25 18:58:51 +0300

    Fix GitHub bug

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+    }
+}
